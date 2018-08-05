cae280aefc9e2a51a1a3b075c009874a19cbf896
Ian Videyko <ian.videyko@gmail.com>
2018-08-05 18:38:13 -0400

    added mongoose config vars for heroku

--- server.js
+++ server.js
@@ -3,31 +3,23 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 var PORT = 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/newsscraper");
 
 require("./routes/API.js")(app);
 
-// Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
